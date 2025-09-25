0a1d2badaa88ee1f4262a35acdb035ce4b4ca285
Guilherme Serraglio <128415162+GuilhermeSerraglio@users.noreply.github.com>
2025-09-25 11:43:30 -0400

    Allow both admin email addresses

--- duo-parfum-pro/api/products.js
+++ duo-parfum-pro/api/products.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
 
-const ADMIN_EMAILS = (process.env.ADMIN_EMAILS || "guilhermeserraglio@gmail.com")
+const ADMIN_EMAILS = (process.env.ADMIN_EMAILS ||
+  "guilhermeserraglio03@gmail.com,guilhermeserraglio@gmail.com")
   .split(",")
   .map((email) => email.trim().toLowerCase())
   .filter(Boolean);
